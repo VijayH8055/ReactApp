61f8ce030257a8303be0d7100d508b4cb0fd298f
VijayH8055 <58595859+VijayH8055@users.noreply.github.com>
2021-06-26 17:34:57 +0530

    ds

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Forest Photogrpahy</h1>
+      <p>
+        A basic web React.Js Bootcamp will provide you so much amount of
+        knowledge about webdevelopment using react framework and you'll learn
+        lot of knowlwdge.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
