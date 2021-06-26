c7e4570bf5c6fcf49e7c4b4248548d496f264c15
VijayH8055 <58595859+VijayH8055@users.noreply.github.com>
2021-06-26 12:34:16 +0530

    text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <p>CopyRight by ShapeAI @ {new Date().getFullYear()}</p>
+      <p>CopyRight by Vijay @ {new Date().getFullYear()}</p>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1>ShapeAI Bootcamp</h1>
+      <h1>Vijay Clicks</h1>
     </header>
   );
 }
