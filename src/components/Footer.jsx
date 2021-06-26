d7833513034c7b03a0bf6150699bd99532589113
VijayH8055 <58595859+VijayH8055@users.noreply.github.com>
2021-06-26 10:32:03 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <header>
+      <p>CopyRight by ShapeAI @ {new Date().getFullYear()}</p>
+    </header>
+  );
+}
+
+export default Footer;
